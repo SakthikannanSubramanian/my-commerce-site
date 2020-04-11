82665379414e77d6b09c553bb270b627a936628c
Sakthikannan Subramanian <sakthikannan.subramanian@waitrose.co.uk>
2020-04-11 19:55:22 +0100

    Form components added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import HomePage from "./components/homepage/homepage.component";
 import ShopPage from "./components/shop/shop-component";
 import Header from "./components/header/header.component";
+import Authentication from "./components/authentication/authentication.component";
+
 import { Route, Switch } from "react-router-dom";
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/authentication" component={Authentication} />
       </Switch>
     </div>
   );
---
+++ src/components/authentication/authentication.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./authentication.styles.scss";
+
+import SignIn from "../sign-in/sign-in.component";
+
+const Authentication = () => (
+  <div>
+    <SignIn />
+  </div>
+);
+
+export default Authentication;
