533e62f0f2beb740a3b106dff586ed2e6a019f0f
Sakthikannan Subramanian <sakthikannan.subramanian@waitrose.co.uk>
2020-04-12 19:12:00 +0100

    Adding redux, react and related actions

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,14 @@
+import { INITIAL_STATE } from "./user-initial-state";
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
