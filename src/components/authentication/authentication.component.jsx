c7a058005a3f279835a1c2ec25da3d1511f3a683
Sakthikannan Subramanian <sakthikannan.subramanian@waitrose.co.uk>
2020-04-12 13:20:22 +0100

    Sign Up method added with the help of firestore db and fire base

--- src/components/authentication/authentication.component.jsx
+++ src/components/authentication/authentication.component.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./authentication.styles.scss";
 
 import SignIn from "../sign-in/sign-in.component";
+import SignUp from "../sign-up/sign-up.component";
 
 const Authentication = () => (
-  <div>
+  <div className="authentication">
     <SignIn />
+    <SignUp />
   </div>
 );
 
